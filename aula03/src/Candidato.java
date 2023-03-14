d2d0285d3a7aa8b15b3d78a2472788380263e0e3
carlos-rafael <carlosrafaelgit@gmail.com>
2023-03-14 07:42:52 -0300

    INclusao de classe candidato

---
+++ aula03/src/Candidato.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+
+public class Candidato {
+    public String Nome;
+    public LocalDate data_nascimento;
+    public String email;
+    public int celular;
+    public String escolaridade;
+    public String funcao;
+    public int pretensao_salarial;
+    public boolean empregado;
+    public boolean estagio;
+}
